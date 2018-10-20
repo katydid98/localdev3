8197f333371008c20098fd5d0f3cb85f016bb17c
Katie Nguyen <kun201@nyu.edu>
2018-10-20 15:10:00 -0400

    Update urls.py

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -6,9 +6,7 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^question_form$', views.QuestionChoiceCreate.as_view(), name='question'),
-    # url(r'^question_form/choices/$', views.ChoicesCreate.as_view(), name='choices'),
-    # url(r'^successpage$', views.SuccessView.as_view(), name='successpage'),
+    url(r'^question$', views.QuestionCreate.as_view(), name='question'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
