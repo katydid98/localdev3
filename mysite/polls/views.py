b52359a0d7cfffbc7f25e19c70a4d3813ff709dc
Katie Nguyen <kun201@nyu.edu>
2018-10-22 19:03:41 -0400

    Update views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,35 +1,33 @@
-
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
+from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 from .models import Choice, Question, Company
 from django.views.generic.edit import CreateView
-from .forms import QuestionForm, ChoiceInlineFormSet
 from django.core.urlresolvers import reverse_lazy
-# from django.shortcuts import redirect
+from polls.forms import QuestionForm, ChoiceInlineFormSet
 import json
 
 
-# def get_companies(request):
-#     if request.is_ajax():
-#         q = request.GET.get('term', '')
-#         companies = Company.objects.filter(name__startswith=q)
-#         results = []
-#         for c in companies:
-#             company_json = {}
-#             company_json['id'] = c.pk
-#             company_json['label'] = c.name
-#             company_json['value'] = c.name
-#             results.append(company_json)
-#         data = json.dumps(results)
-#     else:
-#         data = 'fail'
-#     mimetype = 'application/json'
-#     return HttpResponse(data, mimetype)
+def company_autocomplete(request):
+    if request.is_ajax():
+        q = request.GET.get('term', '')
+        companies = Company.objects.filter(name__startswith=q)
+        results = []
+        for company in companies:
+            company_json = {}
+            company_json['id'] = company.pk
+            company_json['label'] = company.name
+            company_json['value'] = company.name
+            results.append(company_json)
+        data = json.dumps(results)
+    else:
+        data = 'why im i failing??'
+    mimetype = 'application/json'
+    return HttpResponse(data, mimetype)
 
 
 class QuestionCreate(CreateView):
@@ -44,12 +42,10 @@ class QuestionCreate(CreateView):
         and its inline formsets.
         """
         self.object = None
-        form_class = self.get_form_class()
-        form = self.get_form(form_class)
+        form = QuestionForm()
         choice_form = ChoiceInlineFormSet()
         return self.render_to_response(
-            self.get_context_data(form=form,
-                                  choice_form=choice_form))
+            self.get_context_data(form=form, choice_form=choice_form))
 
     def post(self, request, *args, **kwargs):
         """
@@ -58,8 +54,7 @@ class QuestionCreate(CreateView):
         validity.
         """
         self.object = None
-        form_class = self.get_form_class()
-        form = self.get_form(form_class)
+        form = QuestionForm(self.request.POST)
         choice_form = ChoiceInlineFormSet(self.request.POST)
         if (form.is_valid() and choice_form.is_valid()):
             return self.form_valid(form, choice_form)
@@ -83,8 +78,8 @@ class QuestionCreate(CreateView):
         If form is invalid, re-renders the context data with the
         data-filled forms and errors.
         """
-        return self.render_to_response(self.get_context_data(form=form,
-                                       choice_form=choice_form))
+        return self.render_to_response(
+            self.get_context_data(form=form, choice_form=choice_form))
 
 
 class IndexView(generic.ListView):
@@ -135,3 +130,4 @@ def vote(request, question_id):
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results',
                                             args=(question.id,)))
+
